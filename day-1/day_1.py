8772fd0675babc312924d0451fdaa9ef0a183b14
Lohith <pillalohith45@gmail.com>
2025-10-01 10:27:12 -0400

    Add Day 1 solution: first unique character

--- day-1/day_1.py
+++ day-1/day_1.py
@@ -3,10 +3,10 @@ class Solution(object):
         # Step 1: create a dictionary (hash map) to count characters
         counts = {}
         for ch in s:
-            counts[ch] = counts.get(ch, 0) + 1
+            counts[ch] = counts.get(ch, 0) + 1  # .get is a function method  we use 0 for when there is no exist then return 0
 
         # Step 2: check from left to right for the first unique char
-        for i, ch in enumerate(s):
+        for i, ch in enumerate(s):  # enumerate ues's for returns both index an value 
             if counts[ch] == 1:
                 return i
 
